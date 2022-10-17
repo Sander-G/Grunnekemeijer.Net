f0a4f8abee8ed3b18fb3602834c69cd582218c7c
Sander Grunnekemeijer <sander@grunnekemeijer.net>
2022-10-17 16:23:47 +0200

    Last Check-in date fixed

--- @latest/src/components/TypeWriter.jsx
+++ @latest/src/components/TypeWriter.jsx
@@ -10,7 +10,32 @@ import { useState, useEffect } from 'react'
 
 
 
+
 export default function TypeWriter() {
+    const weekDay = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+    const monthName = ["Jan", "Feb","Mar", "Apr", "May", "Jun", "Jul", "Aug","Sep", "Oct", "Nov", "Dec"]
+ 
+    const [currentDate, setCurrentDate] = useState('');
+    useEffect(() => {
+        const d = new Date();
+        let day = weekDay[d.getDay()];
+        const m = new Date();
+        let month = monthName[m.getMonth()]
+        const date = new Date().getDate(); //Current Date
+       
+   
+
+        const hours = new Date().getHours(); //Current Hours
+        const min = new Date().getMinutes(); //Current Minutes
+        const sec = new Date().getSeconds(); //Current Seconds
+       
+
+        setCurrentDate(
+            day + ' ' + month + ' ' + date 
+            + ' ' + hours + ':' + min + ':' + sec
+        );
+    }, []);
+
 
     const [ip, setIp] = useState('');
     //creating function to load ip address from the API
@@ -19,23 +44,20 @@ export default function TypeWriter() {
         console.log(res.data);
         setIp(res.data.IPv4)
     }
-
     useEffect(() => {
         //passing getData method to the lifecycle method
         getData()
-
     }, [])
 
     return (
 
         <>
             <Wrapper>
-
-                <span className='terminal'>Last Check-in: Mon Oct 17 13:26:11 from {ip} </span>
+                <span className='terminal'>Last Check-in: {currentDate} from {ip} </span>
             </Wrapper>
             <Wrapper>
 
-                <span className='terminal'>~ᛇნ:&nbsp; </span>
+                <span className='terminal'>SG:\&nbsp; </span>
                 <TypeIt className="terminal"
                     getBeforeInit={(instance) => {
                         instance.type("And now for something completely").pause(300).delete(10).pause(800).type("not so completely different.");
