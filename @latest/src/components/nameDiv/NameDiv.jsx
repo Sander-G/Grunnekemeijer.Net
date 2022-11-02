2eb28635ebe61b43f367e453a0e3825e5bd7a513
Sander <sander@grunnekemeijer.net>
2022-11-02 22:37:41 +0100

    flexible name div

---
+++ @latest/src/components/nameDiv/NameDiv.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Container, 
+  G,R,U,N,E,K,M,I,J, DotNet} from "./NameDiv.styled"
+
+export default function NameDiv() {
+  return (
+    <Container>
+
+    <G>G</G>
+    <R>R</R>
+    <U>U</U>
+    <N>N</N>
+    <N>N</N>
+    <E>E</E> 
+    <K>K</K>
+    <E>E</E>
+    <M>M</M>
+    <E>E</E>
+    <I>I</I>
+    <J>J</J>
+    <E>E</E>
+    <R>R</R>
+    <DotNet>.NET</DotNet>
+    </Container>
+
+  )
+};
+
+
+
---
+++ @latest/src/components/nameDiv/NameDiv.styled.jsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components'
+
+
+export const Container = styled.div`
+display: flex;
+max-width: 100vw;
+height: auto;
+flex-wrap: wrap;
+`
+export const G = styled.div`
+display:flex;
+font-size: 12rem;
+letter-spacing: 0.4rem;
+font-weight: 800;
+border: 0.5px solid hotpink;
+height: auto;
+width: fit-content;
+`
+// export const R = styled.div`
+// display: flex;
+// height: auto;
+// min-width: fit-content;
+// writing-mode: vertical-lr;
+// font-size: 6rem;
+// letter-spacing: 0.03em;
+// font-weight: 800;
+// justify-content: center ;
+// border: 0.5px solid hotpink;
+// `
+
+export const R = styled(G)`
+`
+export const U = styled(G)`
+`
+export const N = styled(G)`
+`
+export const E = styled(G)`
+`
+export const K = styled(G)`
+`
+export const M = styled(G)`
+`
+export const I = styled(G)`
+`
+export const J = styled(G)`
+`
+export const DotNet = styled.div`
+display: flex;
+height: auto;
+min-width: fit-content;
+writing-mode: vertical-lr;
+font-size: 6rem;
+letter-spacing: -0.1rem;
+font-weight: 800;
+justify-content: center ;
+border: 0.5px solid hotpink; 
+`
