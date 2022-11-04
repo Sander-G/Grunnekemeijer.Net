52a96ec2e5db4616c443391c9bdef953c72b5c56
Sander <sander@grunnekemeijer.net>
2022-11-04 22:06:10 +0100

    navbar

--- Grunnekemeijer.net/src/components/navbar/Navbar.jsx
+++ Grunnekemeijer.net/src/components/navbar/Navbar.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import DarkModeBtn from '../darkmodeBtn/DarkModeBtn'
 import Flashlight from '../flashlight/flashlight'
 import { Container } from './navbar.styled'
-
 export default function Navbar() {
   return (
     <>
     <Container>
     <DarkModeBtn/>
     <Flashlight/>
+
     </Container>
     
     </>
