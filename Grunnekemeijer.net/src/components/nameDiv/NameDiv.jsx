81567a4c3ad9f020a9386985b6a9b85d68cfbc19
Sander <sander@grunnekemeijer.net>
2022-11-08 12:49:54 +0100

    uifx uninstall/cleanup

--- Grunnekemeijer.net/src/components/nameDiv/NameDiv.jsx
+++ Grunnekemeijer.net/src/components/nameDiv/NameDiv.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import UIfx from 'uifx'
-import tickMp3 from './../../assets/tick.mp3'
+
 
 import {
   Container,
@@ -9,19 +8,13 @@ import {
 
 export default function NameDiv() {
 
-  const tick = new UIfx(
-    tickMp3,
-    {
-      volume: 0.5, // value must be between 0.0 ⇔ 1.0
-      throttleMs: 50
-    }
-  )
+
 
 
   return (
     <Container className='nameDiv'>
 
-      <G className='G' onMouseOver={tick.play}>G</G>
+      <G className='G'>G</G>
       <R className='R'>R</R>
       <U className='U'>U</U>
       <N className='N1'>N</N>
