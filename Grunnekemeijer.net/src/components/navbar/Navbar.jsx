e05b28a7633bfec98455fd778e6e3cd82f700888
Sander <sander@grunnekemeijer.net>
2022-11-10 22:54:25 +0100

    hamburger usestate

--- Grunnekemeijer.net/src/components/navbar/Navbar.jsx
+++ Grunnekemeijer.net/src/components/navbar/Navbar.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Container, LeftContainer, RightContainer  } from './Navbar.styled'
 import DarkModeBtn from '../darkmodeBtn/DarkModeBtn'
 import Flashlight from '../flashlight/flashlight'
 import Hamburger from '../hamburger/Hamburger'
 import { Menu } from '../menu/Menu'
-
-
+ 
+const [menuActive, setmMenuActive] = useState(false)
 export default function Navbar() {
   return (
     <>
