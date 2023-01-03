b13c6679cfbfc2f869d4e71963d8eaa154c392e2
Sander - iMac <Sander@Grunnekemeijer.net>
2023-01-03 11:17:23 +0100

    router

--- Grunnekemeijer.net/src/App.jsx
+++ Grunnekemeijer.net/src/App.jsx
@@ -1,25 +1,42 @@
 
 // import TypeWriter from './components/TypeWriter'
-import NameDiv from './components/nameDiv/NameDiv'
+// import NameDiv from './components/nameDiv/NameDiv'
 import GlobalStyle from './globalStyles'
-import Navbar from './components/navbar/Navbar'
-import Socials from './components/socials/Socials'
+// import Navbar from './components/navbar/Navbar'
+// import Socials from './components/socials/Socials'
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Info from './pages/Info'
+import Portfolio from './pages/Portfolio'
 
 
+const App = () => {
 
 
-  const App = () => {
-
-  
 
   return (
     <div className="App">
-    
-     <GlobalStyle />
-     <Navbar/>
-     {/* <TypeWriter/> */}
-     <NameDiv />
-    <Socials />
+      <GlobalStyle />
+
+      <BrowserRouter>
+
+      <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/info' element={<Info />}/>
+      <Route path='/portfolio' element={<Portfolio />}/>
+
+      </Routes>
+
+        {/* <Navbar /> */}
+        {/* <TypeWriter/> */}
+        {/* <NameDiv /> */}
+        {/* <Socials /> */}
+
+
+      </BrowserRouter>
+
+
     </div>
   )
 }
---
+++ Grunnekemeijer.net/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../components/navbar/Navbar'
+import NameDiv from '../components/nameDiv/NameDiv'
+import Socials from '../components/socials/Socials'
+
+export default function Home() {
+    return (
+        <>
+        <Navbar />
+        <NameDiv />
+        <Socials />
+        </>
+    )
+}
