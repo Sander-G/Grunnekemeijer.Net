7cb50f59150453b20c0a94131c61bf6136568364
Sander - MacBook Pro <Sander@Grunnekemeijer.net>
2023-01-04 14:24:54 +0100

    clicky name div ui sounds

--- Grunnekemeijer.net/src/components/nameDiv/NameDiv.jsx
+++ Grunnekemeijer.net/src/components/nameDiv/NameDiv.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import UIFx from 'uifx';
+import sound from '../../assets/tick.mp3';
 import {
   Container,
   G, R, U, N, E, K, M, I, J, DotNet
@@ -7,27 +8,26 @@ import {
 
 export default function NameDiv() {
 
-
+let beep = new UIFx(sound)
 
 
   return (
     <Container className='nameDiv'>
-
-      <G className='G'>G</G>
-      <R className='R'>R</R>
-      <U className='U'>U</U>
-      <N className='N1'>N</N>
-      <N className='N2'>N</N>
-      <E className='E'>E</E>
-      <K className='K'>K</K>
-      <E className='E'>E</E>
-      <M className='M'>M</M>
-      <E className='E'>E</E>
-      <I className='I'>I</I>
-      <J className='J'>J</J>
-      <E className='E'>E</E>
-      <R className='R'>R</R>
-      <DotNet>.NET</DotNet>
+      <G className='G'onMouseEnter={()=>beep.play()}>G</G>
+      <R className='R'onMouseEnter={()=>beep.play()}>R</R>
+      <U className='U'onMouseEnter={()=>beep.play()}>U</U>
+      <N className='N1'onMouseEnter={()=>beep.play()}>N</N>
+      <N className='N2'onMouseEnter={()=>beep.play()}>N</N>
+      <E className='E'onMouseEnter={()=>beep.play()}>E</E>
+      <K className='K'onMouseEnter={()=>beep.play()}>K</K>
+      <E className='E'onMouseEnter={()=>beep.play()}>E</E>
+      <M className='M'onMouseEnter={()=>beep.play()}>M</M>
+      <E className='E'onMouseEnter={()=>beep.play()}>E</E>
+      <I className='I'onMouseEnter={()=>beep.play()}>I</I>
+      <J className='J'onMouseEnter={()=>beep.play()}>J</J>
+      <E className='E'onMouseEnter={()=>beep.play()}>E</E>
+      <R className='R'onMouseEnter={()=>beep.play()}>R</R>
+      <DotNet onMouseEnter={()=>beep.play()}>.NET</DotNet>
 
     </Container>
 
