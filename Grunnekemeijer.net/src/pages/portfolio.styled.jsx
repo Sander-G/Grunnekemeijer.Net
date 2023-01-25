52725ad817d0e28b43a68ef2e0c8a0be6e9a1e6c
Sander - MacBook Pro <Sander@Grunnekemeijer.net>
2023-01-25 20:57:52 +0100

    portfolio initial letter & spacing

--- Grunnekemeijer.net/src/pages/portfolio.styled.jsx
+++ Grunnekemeijer.net/src/pages/portfolio.styled.jsx
@@ -3,8 +3,8 @@ import styled from 'styled-components'
 export const Header = styled.div`
     display: flex;
     width: 100vw;
-    height: 3 rem;
-    padding: 1.5rem;
+    height: 2 rem;
+    padding: 1rem;
     align-items: center;
     justify-content: center;
     font-size: 18px;
@@ -14,16 +14,16 @@ export const Container = styled.div`
   position: relative;
   grid-template-columns: 1fr 3fr;
   grid-template-rows: repeat(6, minmax(100px, 1fr));
-  grid-column-gap: 2rem;
-  grid-row-gap: 2rem;
-  margin: 1rem 1rem 1rem 1rem;
+  grid-column-gap: 1rem;
+  grid-row-gap: 1rem;
+  margin: 1rem 1rem;
   
 
   @media (max-width: 1024px) {
     grid-template-columns: 1fr;
     grid-template-rows: repeat(12, minmax(50px, 1fr));
-    grid-row-gap: 0rem;
-    grid-column-gap: 0rem;
+    grid-row-gap: 1rem;
+    grid-column-gap: 1rem;
   }
 `;
 
@@ -37,6 +37,7 @@ export const StyledImage = styled.img`
   justify-self: center;
   border: 1px solid currentColor;
   border-radius: 5px;
+ 
 
 
   @media (max-width: 1024px) {
@@ -46,12 +47,26 @@ export const StyledImage = styled.img`
   max-height: auto;
   }
 `;
-export const Text = styled.div`
+export const Text = styled.p`
   grid-column: 2 / 3;
   grid-row: span 1;
   font-size: 16px;
   text-align: justify;
-  margin: 1rem 1rem 1rem 1rem;
+  margin: 0rem 1rem;
+  
+  &:first-letter {
+  /* font-family: "Merriweather", serif; */
+  initial-letter: 4.5 4;
+  font-weight: bold;
+  line-height: 1;
+  font-size: 22px;
+  /* margin-right: 0.5rem; */
+  /* color: hotpink; */
+  /* background: #be4bdb; */
+  padding: 0.1rem;
+  /* border-radius: 5px; */
+  /* box-shadow: 0.5rem 0.5rem 0 #3b5bdb; */
+}
   @media (max-width: 1024px) {
     grid-column: 1 / 2;
     grid-row: span 1;
