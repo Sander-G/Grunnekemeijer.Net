9edec676b51edc45aa9ae2656f00a74d8fba1e6f
Sander - iMac <Sander@Grunnekemeijer.net>
2023-02-22 14:20:45 +0100

    typewriter styled component

--- Grunnekemeijer.net/src/components/typewriter/TypeWriter.jsx
+++ Grunnekemeijer.net/src/components/typewriter/TypeWriter.jsx
@@ -1,83 +1,96 @@
-import React from 'react';
-import TypeIt from 'typeit-react';
-import styled from 'styled-components';
-import axios from 'axios'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
+import TypeIt from "typeit-react";
+import axios from "axios";
+import { Wrapper } from "./TypeWriter.styled";
 
 export function TypeWriter() {
-    const weekDay = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
-    const monthName = ['Jan', 'Feb','Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug','Sep', 'Oct', 'Nov', 'Dec'];
- 
-    const [currentDate, setCurrentDate] = useState('');
-    const [lastVisit, setLastVisit] = useState('');
-    const [ip, setIp] = useState('');
-  
- 
-    useEffect(() => {
+  const weekDay = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const monthName = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
-      
-        const d = new Date();
-        const day = weekDay[d.getDay()];
-        const m = new Date();
-        const month = monthName[m.getMonth()]
-        const date = new Date().getDate(); 
-        const hours = new Date().getHours(); 
-        const min = new Date().getMinutes();
-        const sec = new Date().getSeconds(); 
-        const minutes = ('0' + min).slice(-2);
-        const seconds = ('0' + sec).slice(-2);
-       
+  const [currentDate, setCurrentDate] = useState("");
+  const [lastVisit, setLastVisit] = useState("");
+  const [ip, setIp] = useState("");
 
-        setCurrentDate(
-      day + ' ' + month + ' ' + date + ' ' + hours + ':' + minutes + ':' + seconds
+  useEffect(() => {
+    const d = new Date();
+    const day = weekDay[d.getDay()];
+    const m = new Date();
+    const month = monthName[m.getMonth()];
+    const date = new Date().getDate();
+    const hours = new Date().getHours();
+    const min = new Date().getMinutes();
+    const sec = new Date().getSeconds();
+    const minutes = ("0" + min).slice(-2);
+    const seconds = ("0" + sec).slice(-2);
+
+    setCurrentDate(
+      day +
+        " " +
+        month +
+        " " +
+        date +
+        " " +
+        hours +
+        ":" +
+        minutes +
+        ":" +
+        seconds
     );
   }, []);
-       
 
-         useEffect(() => {
-           const lastVisitDate = JSON.parse(localStorage.getItem("lastVisit"));
-           const lastIpAddress = JSON.parse(localStorage.getItem("lastIp"));
-           console.log(lastVisitDate);
-           if (lastVisitDate) {
-             setLastVisit(lastVisitDate);
-           }
+  useEffect(() => {
+    const lastVisitDate = JSON.parse(localStorage.getItem("lastVisit"));
+    const lastIpAddress = JSON.parse(localStorage.getItem("lastIp"));
+    console.log(lastVisitDate);
+    if (lastVisitDate) {
+      setLastVisit(lastVisitDate);
+    }
 
-           if (lastIpAddress) {
-             setIp(lastIpAddress);
-           }
+    if (lastIpAddress) {
+      setIp(lastIpAddress);
+    }
 
-           const getData = async () => {
-             const res = await axios.get("http://www.geolocation-db.com/json/");
-             setIp(res.data.IPv4);
+    const getData = async () => {
+      const res = await axios.get("http://www.geolocation-db.com/json/");
+      setIp(res.data.IPv4);
 
-             localStorage.setItem("lastVisit", JSON.stringify(currentDate));
-             console.log(lastVisit);
-             localStorage.setItem("lastIp", JSON.stringify(res.data.IPv4));
-             
-           };
+      localStorage.setItem("lastVisit", JSON.stringify(currentDate));
+      console.log(lastVisit);
+      localStorage.setItem("lastIp", JSON.stringify(res.data.IPv4));
+    };
 
-           getData();
-         }, [currentDate]);
+    getData();
+  }, [currentDate]);
 
-
-    return (
-      <>
-        <Wrapper>
-        { (lastVisit ? (
-
-          
-            <span className="terminal">
-              You were last here on {lastVisit} from {ip}
-            </span>
-          ) : (
-            <span className="terminal">
-              This is your first visit, your IP number is: {ip}
-            </span>
-          ))}
-        </Wrapper> 
-        <Wrapper>
-          <span className="terminal">S:\&nbsp; </span>
-          {/* <TypeIt className='terminal'
+  return (
+    <>
+      <Wrapper>
+        {lastVisit ? (
+          <span className="terminal">
+            You were last here on {lastVisit} from {ip}
+          </span>
+        ) : (
+          <span className="terminal">
+            This is your first visit, your IP number is: {ip}
+          </span>
+        )}
+      </Wrapper>
+      <Wrapper>
+        <span className="terminal">S:\&nbsp; </span>
+        {/* <TypeIt className='terminal'
                     getBeforeInit={(instance) => {
                         instance.type('And now for something completely').pause(300).delete(10).pause(800).type('not so completely different.')
                         ;
@@ -86,30 +99,19 @@ export function TypeWriter() {
 
                 /> */}
 
-          <TypeIt
-            className="terminal"
-            options={{
-              strings: [
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae at dignissimos veritatis qui veniam temporibus velit placeat! Suscipit libero laudantium commodi vel excepturi non, reiciendis delectus velit expedita deleniti cum. Vero debitis aliquam amet possimus repellat ut, odio accusantium eos nisi molestias laudantium. Veritatis molestias eveniet vel neque molestiae nostrum ipsum omnis voluptates facere quo animi, doloribus, dignissimos dolore quidem eligendi error necessitatibus. Corrupti assumenda provident delectus placeat quasi tenetur voluptate cupiditate ut explicabo, exercitationem soluta id sed nisi ullam.",
-              ],
-              speed: 1,
-              waitUntilVisible: true,
-            }}
-          />
-        </Wrapper>
-      </>
-    );
+        <TypeIt
+          className="terminal"
+          options={{
+            strings: [
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae at dignissimos veritatis qui veniam temporibus velit placeat! Suscipit libero laudantium commodi vel excepturi non, reiciendis delectus velit expedita deleniti cum. Vero debitis aliquam amet possimus repellat ut, odio accusantium eos nisi molestias laudantium. Veritatis molestias eveniet vel neque molestiae nostrum ipsum omnis voluptates facere quo animi, doloribus, dignissimos dolore quidem eligendi error necessitatibus. Corrupti assumenda provident delectus placeat quasi tenetur voluptate cupiditate ut explicabo, exercitationem soluta id sed nisi ullam.",
+            ],
+            speed: 1,
+            waitUntilVisible: true,
+          }}
+        />
+      </Wrapper>
+    </>
+  );
 }
 
-const Wrapper = styled.div`
- display: flex;
- align-items: center;
- flex-direction: row;
- padding: 0.25em 1rem;
- 
-    align-items: baseline;
-    justify-content: flex-start;
-    text-align: left;
-    line-height: 1;
-    letter-spacing: 0.8;
-    `
+
---
+++ Grunnekemeijer.net/src/components/typewriter/TypeWriter.styled.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  padding: 0.25em 1rem;
+
+  align-items: baseline;
+  justify-content: flex-start;
+  text-align: left;
+  line-height: 1;
+  letter-spacing: 0.8;
+`;
