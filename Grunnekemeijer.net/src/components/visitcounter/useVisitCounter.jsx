af83117998b3bfbde6e8c51301dff93737172973
Sander - iMac <Sander@Grunnekemeijer.net>
2023-02-23 19:10:54 +0100

    visit counter logic fixed

--- Grunnekemeijer.net/src/components/visitcounter/useVisitCounter.jsx
+++ Grunnekemeijer.net/src/components/visitcounter/useVisitCounter.jsx
@@ -1,25 +1,23 @@
 import { useState, useEffect } from 'react';
 
-
-
 export const useVisitCounter = () => {
-  const [visitCount, setVisitCount] = useState(parseInt(localStorage.getItem('visitCount')) || 0);
+  const [visitCount, setVisitCount] = useState(parseInt(localStorage.getItem('visitCount')) || 1);
   const [lastVisitTime, setLastVisitTime] = useState(localStorage.getItem('lastVisitTime'));
-  const oneHourInMS = 60 * 60 * 1000
+  const oneHour = 60 * 60 * 1000;
 
   useEffect(() => {
     const lastVisit = localStorage.getItem('lastVisitTime');
     const now = new Date().getTime();
-    
-// after one hour the visit expires
-    if (lastVisitTime && now - lastVisitTime > oneHourInMS) {
+
+    // after one hour the current visit expires
+    if (lastVisitTime && now - lastVisit > oneHour) {
       setVisitCount((prevCount) => prevCount + 1);
       localStorage.setItem('visitCount', visitCount + 1);
     }
 
     localStorage.setItem('lastVisitTime', now);
     setLastVisitTime(now);
-  }, []);
+  }, [visitCount]);
 
   return visitCount;
 };
