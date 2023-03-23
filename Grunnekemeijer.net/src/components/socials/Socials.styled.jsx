0ccacdccf0442d8f8dede2c40b585b186df31910
Sander - MacBook Pro <Sander@Grunnekemeijer.net>
2023-03-23 18:43:39 +0100

    mail icon styling

--- Grunnekemeijer.net/src/components/socials/Socials.styled.jsx
+++ Grunnekemeijer.net/src/components/socials/Socials.styled.jsx
@@ -1,40 +1,36 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex;
-    position: fixed;
-    bottom: 0;
-    z-index:0;
-    flex-direction: row;
-    height: 4rem;
-    place-self: flex-end;
-    align-items: center;
-    justify-content: flex-end;
-    margin: 0.25rem 1.25rem;
-   
-   
-   
- 
+  display: flex;
+  position: fixed;
+  bottom: 0;
+  z-index: 0;
+  flex-direction: row;
+  height: 4rem;
+  place-self: flex-end;
+  align-items: center;
+  justify-content: flex-end;
+  margin: 0.25rem 1.25rem;
 
   & a {
-      filter: drop-shadow(1px 1px 0.5px hotpink);
-      margin-inline: 1rem;
-      @media screen and (max-width:1024px) {
-        margin-inline: 0.5rem;
+    filter: drop-shadow(0.5px 0.5px 0.5px hotpink) drop-shadow(-0.5px -0.5px 0.5px hotpink);
+    margin-inline: 1rem;
+    @media screen and (max-width: 1024px) {
+      margin-inline: 0.5rem;
     }
   }
 
-  & svg { 
+  & svg {
     scale: 1.2;
-    @media screen and (max-width:1024px) {
-    scale: 1;
+    @media screen and (max-width: 1024px) {
+      scale: 1;
     }
   }
-`
+`;
 
 
 export const EmailLinkStyled = styled.div`
-  filter: drop-shadow(1px 1px 0.5px hotpink);
+  filter: drop-shadow(0.5px 0.5px 0.5px hotpink) drop-shadow(-0.5px -0.5px 0.5px hotpink);
   margin-inline: 1rem;
   cursor: pointer;
   @media screen and (max-width: 1024px) {
